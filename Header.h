0eb318ba624ec55602e90ec4f260ab9a912b12d5
wonderGold444 <102667061+wonderGold444@users.noreply.github.com>
2022-04-26 20:30:28 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+ class Vector 
+{
+private : 
+	double x;
+	double y;
+	double z;
+public : 
+	Vector(int x, int y, int z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	};
+	Vector(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = 0;
+	};
+	Vector(int x)
+	{
+		this->x = x;
+		this->y = 0;
+		this->z = 0;
+	};
+	Vector()
+	{
+		this->x = 0;
+		this->y = 0;
+		this->z = 0;
+	};
+	void Print()
+	{
+		cout << "X : " << x << "\nY : " << y << "\nZ : " << z << endl;
+	};
+	void SetX(double x)
+	{
+		this->x = x;
+	};
+	void SetY(double y)
+	{
+		this->y = y;
+	};
+	void SetZ(double z)
+	{
+		this->z = z;
+	};
+	Vector operator +(Vector other)
+	{
+		Vector c;
+		c.x = this->x + other.x;
+		c.y = this->y + other.y;
+		c.z = this->z + other.z;
+		return c;
+	};
+	
+};
+ class Circle 
+ {
+ private :
+	 double R;
+	 string color;
+ public:
+	 Circle(double R , string color)
+	 {
+		 this -> R = R;
+		 this -> color = color;
+	 }
+	 Circle (double R)
+	 {
+		 this-> R = R;
+		 this->color = "Без цвета";
+	 }
+	 void Print()
+	 {
+		 setlocale(LC_ALL, "ru");
+		 cout << "Радиус круга R : " << this->R <<endl;
+		 cout << "Цвет круга : " << this->color << endl;
+	 }
+ };
+
---
+++ Main.cpp
@@ -0,0 +1,25 @@
+#include "Header.h"
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	double x, y, z;
+	cout << "¬ведите координаты вектора :" << endl;
+	cout << "x : " ;
+	cin >> x;
+	cout << "y : " ;
+	cin >> y;
+	cout << "z : " ;
+	cin >> z;
+	Vector a(x,y,z);
+	a.Print();
+	double R;
+	string color;
+	cout << "¬ведите радиус круга : ";
+	cin >> R;
+	cout << "¬ведите цвет круга(на английском) :";
+	cin >> color;
+	Circle b(R , color);
+	b.Print();
+
+}
